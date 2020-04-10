9fc0c49a081aedf49ae425844b601d9b65aa4b3e
Aditya <Adi.shah0912@gmail.com>
2020-04-10 13:31:53 +0530

    Growing routes started

--- routes/farmer.js
+++ routes/farmer.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const pool = require("./db");
 
+
+//Dashboard
+/* router.get('/',(req,res)=>{
+
+}); */
+
+//Cultivating
+router.post('/growing',async (req,res)=>{
+    //check if crop is present in the database
+    // const {farmerid,cropid,}
+    // const check = await pool.query("SELECT EXISTS(SELECT 1 FROM crops where farmerid = $1 AND cropid = $2)",[]);
+});
 
 
 module.exports = router;
