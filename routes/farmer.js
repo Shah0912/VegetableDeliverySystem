5b6849db43f288b842bfed6bc7ed40babb80b8b1
Aditya <Adi.shah0912@gmail.com>
2020-04-13 16:43:46 +0530

    Added routes for /farmer/growing

--- routes/farmer.js
+++ routes/farmer.js
@@ -13,19 +13,22 @@ router.use(express.json());
 //Cultivating
 router.post('/growing',async (req,res)=>{
     try {
-        const {farmerid,name,rate,type,farmage,farmsize} = req.body;
-        //console.log(req.body);
-        //console.log(farmerid);
-        const check = await pool.query("SELECT EXISTS(SELECT 1 FROM farmer where farmerid = $1 LIMIT 1);",[farmerid]);
-    
-        console.log(check.rows[0].exists);
-        //if(check.rows[0].exists)
+        const {farmerid,name,rate,type,farmage,farmsize,season} = req.body;
+        console.log(req.body);
+        
+        // Check if farmer exists
+        check = await pool.query("SELECT EXISTS(SELECT 1 FROM farmer where farmerid = $1 LIMIT 1);",[farmerid]);
+        if(check.rows[0].exists)
         {
-
+            console.log("farmer exists proceed");
+            const newCrop = await pool.query("INSERT INTO crop (farmerid,name,rate,type,farmage,farmsize,season) values($1,$2,$3,$4,$5,$6,$7);",[farmerid,name,rate,type,farmage,farmsize,season]);
+            console.log(newCrop.rows);
+            res.json(newCrop);
         }
-        //else
+        else
         {
             console.log("Farmer doesnt exist in the database, Please register first");
+            res.json({"Farmer": "false"});
         }
 
     } catch (error) {
