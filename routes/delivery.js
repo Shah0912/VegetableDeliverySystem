37c2a2be37f42758e0c552cb0e7a9a1ceb4eaf86
Aditya <Adi.shah0912@gmail.com>
2020-04-13 13:55:32 +0530

    Added route for adding a new vehicle
    If there is already a vehicle with same licenceNo or VehicleNo it'll throw an error

--- routes/delivery.js
+++ routes/delivery.js
@@ -1,5 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const pool = require("../db");
+router.use(express.json());
+
+
+
+//Add Vehicle details route
+router.post('/vehicle', async (req,res)=>{
+    try {
+        
+        const {vehicle_no,licence_no,capacity,type} = req.body;
+        //console.log(req.body);
+        //console.log(vehicle_no,licence_no,type,capacity);
+        check = await pool.query("SELECT EXISTS(SELECT 1 FROM vehicles where vehicleno = $1 OR licence_number = $2 LIMIT 1);",[vehicle_no,licence_no]);
+        //console.log(check.rows);
+
+        if(check.rows[0].exists == false)
+        {
+            newVehicle = await pool.query("INSERT INTO vehicles (vehicleno,licence_number,volume_capacity,type) values ($1,$2,$3,$4);",[vehicle_no,licence_no,capacity,type]);
+            //console.log(newVehicle);
+            res.json(newVehicle);
+        }
+        else{
+            console.log("THE VEHICLE IS ALREADY REGISTERED");
+            res.json(check);
+        }
+    
+    } catch (error) {
+        console.error(error.message)
+    }
+});
 
 
 
