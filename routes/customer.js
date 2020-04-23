bab7e79eafe9085b3c8df02cacd0a5caad17c871
Aditya <Adi.shah0912@gmail.com>
2020-04-23 22:39:48 +0530

    Clear flow on how the orders will be handled added

--- routes/customer.js
+++ routes/customer.js
@@ -16,8 +16,21 @@ router.get('/',async(req,res)=>{
 //ADD TO CART
 router.post('/addtocart', async (req,res)=>{
 
+    //IF customer doesnt exist in the cart CREATE the customer's cart
+    //WHEN add to cart happens add the crop id in the temp order
+        //if the crop id exists update the amount
+        //else add new crop id
+    
     console.log(req.body);
 
 });
+//FOR CARTS
+/*
+    *First the order will be stored in a cart : cart and temporder
+    *WHen customer checks out the cart the customer is deleted from temporder and cart
+    *AND IT is added as an order: orders and ordered tables 
+*/
+
+
 
 module.exports = router;
